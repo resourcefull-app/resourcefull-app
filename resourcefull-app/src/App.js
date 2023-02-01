e6bf9a152b04ca6070710f77295d426cfc97f73e
Pradip.Bhume <Pradip.Bhume@e-zest.in>
2023-02-01 17:37:16 +0530

    Login-Register-Reset page with code created - Pradip

--- resourcefull-app/src/App.js
+++ resourcefull-app/src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
-import Login from './Components/Login/Login';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./Components/Login/Login";
+import Register from "./Components/Registration/Register";
+import Reset from "./Components/Reset/Reset";
+import Dashboard from "./Components/Dashboard/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      {/* <h1>Resourcefull App</h1> */}
-      <Login></Login>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login></Login>} />
+          <Route exact path="/register" element={<Register></Register>} />
+          <Route exact path="/reset" element={<Reset></Reset>} />
+          <Route exact path="/dashboard" element={<Dashboard></Dashboard>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- resourcefull-app/src/Components/Login/Login.js
+++ resourcefull-app/src/Components/Login/Login.js
@@ -1,77 +1,56 @@
-import React from "react";
-import "./login.css"
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth, signInWithEmailAndPassword, signInWithGoogle } from "../../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import "./login.css";
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    if (loading) {
+      // maybe trigger a loading screen
+      return;
+    }
+    if (user) navigate("/dashboard");
+  }, [user, loading, navigate]);
   return (
-    <div>
-      <form id="login_form">
-        <div className="text-center mb-3">
-          <p>Sign in with:</p>
-          <button type="button" className="btn btn-link btn-floating mx-1">
-            <i className="fa fa-facebook-f"></i>
-          </button>
-
-          <button type="button" className="btn btn-link btn-floating mx-1">
-            <i className="fa fa-google"></i>
-          </button>
-
-          <button type="button" className="btn btn-link btn-floating mx-1">
-            <i className="fa fa-twitter"></i>
-          </button>
-
-          <button type="button" className="btn btn-link btn-floating mx-1">
-            <i className="fa fa-github"></i>
-          </button>
-        </div>
-
-        <p className="text-center">or:</p>
-
-        <div className="form-outline mb-4">
-          <label className="form-label" htmlFor="loginName">
-            Email or username
-          </label>
-          <input type="email" id="loginName" className="form-control" />
-        </div>
-
-        <div className="form-outline mb-4">
-          <label className="form-label" htmlFor="loginPassword">
-            Password
-          </label>
-          <input type="password" id="loginPassword" className="form-control" />
-        </div>
-
-        <div className="row mb-4">
-          <div className="col-md-6 d-flex justify-content-center">
-            <div className="form-check mb-3 mb-md-0">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                value=""
-                id="loginCheck"
-              />
-              <label className="form-check-label" htmlFor="loginCheck">
-                {" "}
-                Remember me{" "}
-              </label>
-            </div>
-          </div>
-
-          <div className="col-md-6 d-flex justify-content-center">
-            <a href="#!">Forgot password?</a>
-          </div>
-        </div>
-
-        <button type="submit" className="btn btn-primary btn-block mb-4">
-          Sign in
+    <div className="login">
+      <div className="login__container">
+        <input
+          type="text"
+          className="login__textBox"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="E-mail Address"
+        />
+        <input
+          type="password"
+          className="login__textBox"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button
+          className="login__btn"
+          onClick={() => signInWithEmailAndPassword(email, password)}
+        >
+          Login
         </button>
-
-        <div className="text-center">
-          <p>
-            Not a member? <a href="#!">Register</a>
-          </p>
+        <button className="login__btn login__google" onClick={signInWithGoogle}>
+          Login with Google
+        </button>
+        <div>
+          <Link to="/reset">Forgot Password</Link>
         </div>
-      </form>
+        <div>
+          Don't have an account? <Link to="/register">Register</Link> now.
+        </div>
+      </div>
     </div>
   );
 }
-
 export default Login;
